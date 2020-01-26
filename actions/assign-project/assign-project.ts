c344be5f7475d44ee0b731d6d370a669599cdae3
Keivan <me@keivan.io>
2020-01-25 17:26:56 -0800

    more cleanup

--- actions/assign-project/assign-project.ts
+++ actions/assign-project/assign-project.ts
@@ -1,46 +1,11 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { addIssueToColumn } from 'actions/github';
 // import { wait } from './wait'
-
-const token = core.getInput('repo-token');
 
 async function run(): Promise<void> {
   try {
-
-    const octokit = new github.GitHub(token);
-
-    const repo = await octokit.repos.get({ owner: github.context.repo.owner, repo: github.context.repo.repo });
-    const projects = await octokit.projects.listForRepo({ repo: repo.data.name, owner: github.context.repo.owner});
-
-    const projectInfo = projects.data.find(p => p.name == 'Test Project');
-
-    if (!projectInfo) {
-      throw new Error('Couldnt find project');
-    }
-
-
-    const columns = await octokit.projects.listColumns({ project_id: projectInfo.id });
-
-    console.log('found columns ', columns.data);
-    const issue = github.context.issue;
-
-    const is = await octokit.issues.get(issue);
-
-
-
-    octokit.projects.createCard({column_id: columns.data.find(c=>c.name == 'Backlog')?.id!, content_id: is.data.id, content_type: 'Issue'})
-
-    const i = await octokit.issues.get({ issue_number: issue.number, owner: github.context.repo.owner, repo: github.context.repo.repo });
-
-
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
-
-    // core.setOutput('time', new Date().toTimeString())
+    await addIssueToColumn(github.context.issue.number, "Backlog");
   } catch (error) {
     core.setFailed(error)
   }
---
+++ actions/github.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+const token = core.getInput('repo-token');
+const context = github.context;
+
+const gh = new github.GitHub(token);
+
+export async function getRepo() {
+    const repo = await gh.repos.get(context.repo);
+    return repo.data;
+}
+
+export async function getProject(name: string = core.getInput('project')) {
+    const projects = await gh.projects.listForRepo(context.repo);
+    const projectInfo = projects.data.find(p => p.name == name);
+
+    if (!projectInfo) throw new Error(`Project ${name} was not found in ${context.repo}`);
+
+    return projectInfo;
+}
+
+export async function getProjectColumn(name: string = core.getInput('column')) {
+    const project = await getProject();
+    const columns = await gh.projects.listColumns({ project_id: project.id });
+    const col = columns.data.find(c => c.name == name);
+
+    if (!col) throw new Error(`Column ${name} was not found in  ${project.name} ${context.repo}`);
+    return col;
+}
+
+
+export async function addIssueToColumn(issueNumber: number, columnName: string) {
+    const col = await getProjectColumn(columnName);
+    return await gh.projects.createCard({ column_id: col.id, content_id: issueNumber, content_type: 'Issue' })
+}
