e511f2ba9511b270bcb2338085dddfeb42386ecc
Keivan <me@keivan.io>
2020-01-25 17:43:36 -0800

    added logging

--- actions/github.ts
+++ actions/github.ts
@@ -7,6 +7,7 @@ const gh = new github.GitHub(token);
 
 export async function getRepo() {
     const repo = await gh.repos.get(context.repo);
+    console.log('Repo:', repo);
     return repo.data;
 }
 
@@ -15,6 +16,8 @@ export async function getProject(name: string = core.getInput('project')) {
     const projectInfo = projects.data.find(p => p.name == name);
 
     if (!projectInfo) throw new Error(`Project ${name} was not found in ${context.repo}`);
+
+    console.log('Project:', projectInfo);
 
     return projectInfo;
 }
@@ -25,6 +28,8 @@ export async function getProjectColumn(name: string = core.getInput('column')) {
     const col = columns.data.find(c => c.name == name);
 
     if (!col) throw new Error(`Column ${name} was not found in  ${project.name} ${context.repo}`);
+
+    console.log('Col:', col);
     return col;
 }
 
