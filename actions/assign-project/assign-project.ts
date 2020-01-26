6cf5dcd51a9384cab9f503f8d97f27627604f4df
Keivan <me@keivan.io>
2020-01-25 17:29:31 -0800

    updated import

--- actions/assign-project/assign-project.ts
+++ actions/assign-project/assign-project.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { addIssueToColumn } from 'actions/github';
+import { addIssueToColumn } from '../github';
 // import { wait } from './wait'
 
 async function run(): Promise<void> {
